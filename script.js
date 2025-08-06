f4c6908c4d2a5c71f691a59aefa9aa64418041dc
Oscar Milligan <milliganoscar2007@gmail.com>
2025-08-06 20:07:40 +0100

    added functionality to expand a card

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const focus = (card) => {
+
+  const focus_card = (card) => {
+    if (card.classList.contains("card--unfocused")){
+        const placeholder = document.createElement("div");
+        placeholder.classList.add("hidden")
+        placeholder.id = "placeholder"
+        card.insertAdjacentElement("beforebegin", placeholder);
+        card.classList.add('card--focused');
+        card.classList.remove('card--unfocused');
+        
+    }
+
+    else{
+        const placeholder = document.getElementById("placeholder");
+        placeholder.remove();
+        card.classList.remove('card--focused');
+        card.classList.add('card--unfocused');
+    }
     
 }
