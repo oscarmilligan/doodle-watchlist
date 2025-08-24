f4c116fb1e95c1a8bc639989c8374e2826f51627
Oscar Milligan <milliganoscar2007@gmail.com>
2025-08-24 19:34:15 +0100

    Improved the expand animation but the shrink animation still does not work

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
   const focus_card = (card) => {
-    if (card.classList.contains("card--unfocused")){
+    if (card.classList.contains("card--unfocused") || card.classList.contains("card--onload")){
+        // get card position
+        const leftpos = card.offsetLeft;
+        const toppos = card.offsetTop;
+        const width = card.width;
+        const height = card.height;
+        console.log(leftpos+"px",toppos +"px")
+        card.style.setProperty('--card-left', leftpos+"px");
+        card.style.setProperty('--card-top', toppos+"px");
+
+        // insert placeholder to grid
         const placeholder = document.createElement("div");
-        placeholder.classList.add("hidden")
-        placeholder.id = "placeholder"
+        placeholder.classList.add("hidden");
+        placeholder.id = "placeholder";
         card.insertAdjacentElement("beforebegin", placeholder);
-        card.classList.add('card--focused');
-        card.classList.remove('card--unfocused');
+        //fix card
+        card.style.position = "fixed";
+        card.style.left = leftpos +"px";
+        card.style.top = toppos +"px";
+        // edit classes
         
+        card.classList.contains("card--unfocused") ? card.classList.remove('card--unfocused') : card.classList.remove('card--onload');
+        card.classList.add('card--focused');
     }
 
     else{
+        // get placeholder position
         const placeholder = document.getElementById("placeholder");
+        const leftpos = placeholder.offsetLeft;
+        const toppos = placeholder.offsetTop;
+        console.log(leftpos+"px",toppos +"px")
+        card.style.setProperty('--card-left', leftpos+"px");
+        card.style.setProperty('--card-top', toppos+"px");
+
+        // remove placeholder
         placeholder.remove();
+        
+        // unfix card
+        card.style.position = "relative";
+        
         card.classList.remove('card--focused');
         card.classList.add('card--unfocused');
+        sleep(1000)
+        void card.style;
     }
     
 }
