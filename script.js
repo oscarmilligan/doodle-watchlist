067f9c3d39b864022342005dbc097fc76d9cb478
Oscar Milligan <milliganoscar2007@gmail.com>
2025-08-28 19:12:04 +0100

    added and created transitions for a canvas area

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-
-  const focus_card = (card) => {
-    if (card.classList.contains("card--unfocused") || card.classList.contains("card--onload")){
+var card_focussed
+const focus_card = (card) => {
+    console.log(card_focussed);
+    if (card_focussed) {
+        unfocus_card(card_focussed);
+    }
+    if (card != card_focussed && (card.classList.contains("card--unfocused") || card.classList.contains("card--onload"))){
         // get card position
         const leftpos = card.offsetLeft;
         const toppos = card.offsetTop;
         const width = card.width;
         const height = card.height;
         console.log(leftpos+"px",toppos +"px")
-        card.style.setProperty('--card-left', leftpos+"px");
-        card.style.setProperty('--card-top', toppos+"px");
 
         // insert placeholder to grid
         const placeholder = document.createElement("div");
@@ -23,28 +25,39 @@
         // edit classes
         card.classList.contains("card--unfocused") ? card.classList.remove('card--unfocused') : card.classList.remove('card--onload');
         card.classList.add('card--focused');
+        card_focussed = card;
 
-
+        // unhide canvas
+        const canvas_area = document.getElementById("canvas-area")
+        canvas_area.classList.remove("hidden")
     }
 
-    else{
-        // get placeholder position
-        const placeholder = document.getElementById("placeholder");
-        const leftpos = placeholder.offsetLeft;
-        const toppos = placeholder.offsetTop;
-        card.style.setProperty('--card-left', leftpos+"px");
-        card.style.setProperty('--card-top', toppos+"px");
-
-        // remove placeholder
-        placeholder.remove();
-        
-        // unfix card
-        card.style.position = "relative";
-        card.style.left = "0";
-        card.style.top = "0";
-
-        card.classList.remove('card--focused');
-        card.classList.add('card--unfocused');
-    }
+    // else{
+    //     //unfocus card
+    //     unfocus_card(card)
+    // }
     
+}
+const unfocus_card = (card = "N/A") => {
+    if (card = "N/A"){
+        card = card_focussed
+    }
+    // get placeholder position
+    const placeholder = document.getElementById("placeholder");
+
+    // remove placeholder
+    placeholder.remove();
+    
+    // unfix card
+    card.style.position = "relative";
+    card.style.left = "0";
+    card.style.top = "0";
+
+    card.classList.remove('card--focused');
+    card.classList.add('card--unfocused');
+    card_focussed = null;
+
+    // hide canvas area
+    const canvas_area = document.getElementById("canvas-area")
+    canvas_area.classList.add("hidden")
 }
